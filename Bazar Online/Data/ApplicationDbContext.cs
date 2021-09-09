17fd0dd4726e21b23e41135bc56a26a42779144f
danielsolari4 <danielsolari4@gmail.com>
2021-09-09 01:30:12 -0300

    Agregar archivos de proyecto.

---
+++ Bazar Online/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Bazar_Online.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bazar_Online.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Bazar Online/Models/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bazar_Online.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public float Precio { get; set; }
+        public int Cantidad { get; set; }
+        public TipoProducto Tipo { get; set; }
+        public State Estado { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public DateTime FechaPublicacion { get; set; }
+
+
+
+        public enum TipoProducto
+        {
+            Botellita,
+            Mate
+        }
+
+        public enum State
+        {
+            Borrador,
+            Publicada,
+            Eliminada
+        }
+
+    }
+
+
+    
+
+}
